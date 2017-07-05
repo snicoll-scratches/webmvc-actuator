b8e67283bfe38b40d50ef5d0d677f87967d1a7ab
Stephane Nicoll <snicoll@pivotal.io>
2017-07-05 14:28:39 +0100

    Initial import

---
+++ src/main/java/com/example/webmvc/actuator/WebmvcActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.webmvc.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmvcActuatorApplication.class, args);
+	}
+}
