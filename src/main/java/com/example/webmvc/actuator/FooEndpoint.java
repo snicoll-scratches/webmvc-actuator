551c79ce88362fbcbd67f49f215107030af010d0
Stephane Nicoll <snicoll@pivotal.io>
2017-07-16 19:00:53 +0200

    Improve Example endpoint

--- src/main/java/com/example/webmvc/actuator/FooEndpoint.java
+++ src/main/java/com/example/webmvc/actuator/FooEndpoint.java
@@ -1,11 +1,14 @@
 package com.example.webmvc.actuator;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.endpoint.Endpoint;
 import org.springframework.boot.endpoint.ReadOperation;
+import org.springframework.boot.endpoint.Selector;
 import org.springframework.boot.endpoint.WriteOperation;
 import org.springframework.stereotype.Component;
 
@@ -26,17 +29,17 @@ public class FooEndpoint {
 	}
 
 	@ReadOperation
-	public Collection<Foo> getAll() {
-		return this.all.values();
+	public List<Foo> getAll() {
+		return new ArrayList<>(this.all.values());
 	}
 
 	@ReadOperation
-	public Foo getFoo(String name) {
+	public Foo getFoo(@Selector String name) {
 		return this.all.get(name);
 	}
 
 	@WriteOperation
-	public void updateFoo(String name, String value) {
+	public void updateFoo(@Selector String name, String value) {
 		this.all.put(name, new Foo(value));
 	}
 
